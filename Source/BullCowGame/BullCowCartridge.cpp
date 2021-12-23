359b2f5dcc30cb8ca77e031b4ccf52ba44eb4f37
Paul Brooker <pdbrooker@gmail.com>
2021-12-23 16:39:41 -0400

    up to lecture 58

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,47 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine(TEXT("Welcome to the Bull|Cows Game! Have fun!"));
+    PrintLine(TEXT("Try guessing the isogram word!"));
+    PrintLine(TEXT("Press enter to begin"));
+
+    SetUpGame();
+
+    // prompt player for guess
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
+    ClearScreen();
 
+    // check player guess
+
+    if (Input == HiddenWord)
+    {
+        PrintLine(TEXT("You win!"));
+    }
+    else
+    {
+        if (Input.Len() != HiddenWord.Len())
+        {
+            PrintLine(TEXT("Your word length is incorrect. Try again!"));
+        }
+        else {
+            PrintLine(TEXT("That is incorrect!"));
+        }
+    }
+   
+    // check if isogram
+    // check character count
+    // check character position
+    // remove life
+    // check if lives > 0
+    // if yes - continue game - show lives left
+    // if no - fail and show word - offer to play again
+}
+    
+void UBullCowCartridge::SetUpGame()
+{
+    HiddenWord = TEXT("word");
+    Lives = 4;
 }
